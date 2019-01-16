9ca21c941e6c9fe2f83bd54eb12d3b6cf8f9541e
Glenn Y. Rolland <glenux@glenux.net>
2019-01-16 15:50:45 +0100

    Update WG icon

---
+++ img/export-to-bitmap.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -u
+set -e
+set -x
+
+for file in *.svg ; do
+        echo "$file"
+
+        for size in 4096 2048 1024 512 256 128 100 64 32 16 ; do
+                smallpng="$(echo "$file" |sed "s/.svg$/-${size}.png/")"
+                echo "$smallpng"
+
+                inkscape \
+                        --without-gui \
+                        "--file=$file" \
+                        "--export-width=${size}" \
+                        "--export-png=${smallpng}"
+        done
+done
+
