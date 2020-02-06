26333dde06b6a8278137cdd9466a40078f23256f
Glenn Y. Rolland <glenux@glenux.net>
2020-02-06 16:33:37 +0100

    Improve icon quality and contrast

--- img/export-to-bitmap.sh
+++ img/export-to-bitmap.sh
@@ -7,15 +7,20 @@ set -x
 for file in *.svg ; do
         echo "$file"
 
+        maxpng="$(echo "$file" |sed "s/.svg$/-max.png/")"
+        inkscape \
+            --without-gui \
+            "--file=$file" \
+            "--export-width=4096" \
+            "--export-png=${maxpng}"
+
         for size in 4096 2048 1024 512 256 128 100 96 64 48 32 16 ; do
                 smallpng="$(echo "$file" |sed "s/.svg$/-${size}.png/")"
                 echo "$smallpng"
 
-                inkscape \
-                        --without-gui \
-                        "--file=$file" \
-                        "--export-width=${size}" \
-                        "--export-png=${smallpng}"
+				convert -filter Lanczos -resize "${size}x${size}" \
+					"$maxpng" \
+					"$smallpng"
         done
 done
 
